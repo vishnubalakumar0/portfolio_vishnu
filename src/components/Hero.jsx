edb1683d6b5ee1c562843c89b47f45a66691ddda
vishnubalakumar0 <vishnubalakumar15@gmail.com>
2025-12-30 12:24:29 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,39 @@
 import pic from "../assets/profile.png";
-import {HERO_CONTENT} from "../constants";
-
+import { HERO_CONTENT } from "../constants";
+import { motion } from "motion/react";
 
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
-          <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-            Vishnu B
-          </h1>
-          <span className="bg-linear-to-r from-pink-300 via-slate-50 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
-            Full stack Developer
-          </span>
-          <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
-            {HERO_CONTENT}
-          </p>
+        <div className="w-full lg:w-1/2 ">
+          <div className="flex flex-col items-center ml-8 lg:items-start">
+            <h1 className="text-6xl font-poppins font-bold leading-tight lg:mb-5">
+              Hi, I'm{" "}
+              <span className="bg-linear-to-r from-purple-400 to-blue-500  text-transparent  bg-clip-text">
+                Vishnu
+              </span>
+            </h1>
+            <span className="bg-linear-to-r from-pink-600 via-red-500 to-pink-500 bg-clip-text text-4xl tracking-tight text-transparent mb-3">
+              Full stack Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 text-neutral-200 tracking-widest  text-lg">
+              {HERO_CONTENT}
+            </p>
+          </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex justify-center">
-              <img height={450} width={450} src={pic} alt="h" className="rounded-2xl"/>
+          <div className="flex  justify-center -mt-10">
+            <img
+              height={500}
+              width={500}
+              src={pic}
+              alt="h"
+              className="rounded-2xl "
+            />
           </div>
         </div>
       </div>
-        
-
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,23 +3,51 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-
-
+import {motion} from "motion/react";
 
 const Nav = () => {
   return (
-        <nav className="mb-20 flex items-center justify-between py-6">
-          <div className="flex shrink-0 items-center">
-              <img src={logo} alt="lo" className="mx-2 w-10" />
-          </div>
-          <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin/>
-            <FaGithub/>
-            <FaInstagram/>
-            <FaXTwitter/>
-          </div>
-        </nav>  
-  )
-}
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex shrink-0 items-center">
+        <img src={logo} alt="lo" className="mx-2 w-10" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <motion.a
+    initial={{ opacity: 1, y: 0 }}
+    whileHover={{ opacity: 1, y: -15 }}
+    transition={{ duration: 0.25 }}
+        href="https://www.linkedin.com/in/vishnubalakumar" target="_blank">
+          <FaLinkedin />
+        </motion.a>
+        <motion.a
+          initial={{ opacity: 1, y: 0 }}
+          whileHover={{ opacity: 1, y: -15 }}
+          transition={{ duration: 0.25 }}
+          href="https://github.com/vishnubalakumar0" target="_blank">
+          {" "}
+          <FaGithub />
+        </motion.a>
 
-export default Nav
+        <motion.a
+          initial={{ opacity: 1, y: 0 }}
+          whileHover={{ opacity: 1, y: -15 }}
+          transition={{ duration: 0.25 }}
+          href="https://www.instagram.com/sweet_boy_vichu" target="_blank">
+          {" "}
+          <FaInstagram />
+        </motion.a>
+
+        <motion.a
+          initial={{ opacity: 1, y: 0 }}
+          whileHover={{ opacity: 1, y: -15 }}
+          transition={{ duration: 0.25 }}
+          href="https://x.com/?lang=en-in" target="_blank">
+          {" "}
+          <FaXTwitter />
+        </motion.a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
